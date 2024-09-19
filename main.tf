682e55057ee97e6199a5f1795c1a8e5e4bd860ed
Kwame <ksasante34@gmail.com>
2024-09-19 16:56:56 -0400

    update

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
-resource "aws_s3_bucket" "sandbucket8133" {
-  bucket = "sandbucket8133"
+resource "aws_s3_bucket" "sandbucket81335" {
+  bucket = "sandbucket81335"
 }
 resource "aws_s3_bucket" "sandbucket81334" {
   bucket = "sandbucket81334"
